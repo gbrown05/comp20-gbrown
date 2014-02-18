aec332f84f2468cfba4495823d9cfd8e44acf464
George Brown <gbrown3593@gmail.com>
2014-02-18 18:01:56 -0500

    started gamejs; added onload to html

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,16 @@
-// Your work goes here...
+// George Brown
+// Comp 20, Assignment #2: Duckhunt Part I
+// 25 Febraury 2014
+// game.js
+
+function draw() {
+	var canvas = document.getElementById("game");
+
+	// Check to make sure browser supports <canvas> 
+	if (canvas.getContext) {
+		ctx = canvas.getContext("2d");
+		sky = "#87CEEB";
+		dirt = "#C96A1B";
+
+	}
+}
