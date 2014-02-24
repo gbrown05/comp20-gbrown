4e5a72300f718ffbd07b3efecd3dad33c87e28eb
George Brown <gbrown3593@gmail.com>
2014-02-24 12:53:01 -0500

    added extra dirt and tree

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -10,49 +10,27 @@ function draw() {
 	if (canvas.getContext) {
 		ctx = canvas.getContext("2d");
 		var img = new Image();
-		ctx.fillStyle = "87CEEB";
-		ctx.fillRect(0, 0, 800, 600);
 		img.onload = function() {
-			//sky = "#87CEEB";
-			//ctx.fillRect(0, 0, 800, 600);
+			ctx.fillStyle = "87CEEB";
+			ctx.fillRect(0, 0, 800, 600);
+			
+			ctx.fillStyle = "C96A1B";
+			ctx.fillRect(0, 563, 800, 37);
+			
+			//dirt, grass, 2 bushes
+			ctx.drawImage(img, 0, 717, 900, 183, 0, 380, 800, 183);
+
+			//tree
+			ctx.drawImage(img, 0, 271, 81, 129, 200, 128, 200, 350);
 
 
-			ctx.drawImage(img, 0, 717, 900, 183, 0, 417, 800, 183);
-
-			//dirt = "#C96A1B";
-			//ctx.fillStyle = dirt;
-			//ctx.drawImage(img,0,12500);
-
-			//ctx.fillStyle = sky;
 
 		}
 		img.src = "assets/duckhunt.png";
-
-
 
 	}
 	else {
 		alert("Your browser doesn't support Duckhunt!")
 	}
 }
-
-
-
-
-/*
-0, 12500
-0, 11755
-12,500, 12,500
-12,500, 11755
-
-DOG:  870,620
-1653,620
-870,0
-1653,0
-*/
-
-
-
-
-
 
